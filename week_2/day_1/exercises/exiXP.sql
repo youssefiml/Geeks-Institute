808decd886e52bdbffd80877643b03cbf06e6884
youssefiml <youssefimlilss10@gmail.com>
2025-09-02 11:56:25 +0100

    frst sql

--- week_2/day_1/exercises/exiXP.sql
+++ week_2/day_1/exercises/exiXP.sql
@@ -6,8 +6,43 @@ CREATE TABLE items (
 
 CREATE TABLE customers (
     customer_id SERIAL PRIMARY KEY,
-    full_name VARCHAR(100),
-    email VARCHAR(100)
+    first_name VARCHAR(50),
+    last_name VARCHAR(100)
 );
 
-INSERT INTO items 
+INSERT INTO items (name, price)
+VALUES 
+    ('Small Desk', 100),
+    ('Large desk', 300),
+    ('Fan', 80);
+
+INSERT INTO customers(first_name, last_name)
+VALUES ('Greg', 'Jones'),
+    ('Sandra', 'Jones'),
+    ('Scitt', 'Scott'),
+    ('Trevor', 'Green'),
+    ('Melanie', 'Johnson');
+
+
+
+SELECT * FROM items;
+
+SELECT *
+FROM items
+WHERE price > 80;
+
+SELECT *
+FROM items
+WHERE price <= 300;
+
+SELECT *
+FROM customers
+WHERE last_name = 'Smith';
+
+SELECT *
+FROM customers
+WHERE last_name = 'Jones';
+
+SELECT *
+FROM customers
+WHERE last_name != 'Scott';
